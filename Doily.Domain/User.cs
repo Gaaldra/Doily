3a69c61cb4061124cb4e0cfcab2395a0dbdaa63a
Gaaldra <gabrielalves.quadra@gmail.com>
2025-10-15 11:04:44 -0300

    feat(domain): adiciona classe User ao domínio
    Foi adicionada a classe `User` ao `Doily.Domain` para representar usuários no sistema.

---
+++ Doily.Domain/User.cs
@@ -0,0 +1,21 @@
+﻿namespace Doily.Domain;
+
+public class User
+{
+    public int ID { get; private set; }
+    public string FirstName { get; private set; } = String.Empty;
+
+    public string LastName { get; private set; } = String.Empty;
+    public string Username { get; private set; } = String.Empty;
+    public string Password { get; private set; } = String.Empty;
+
+    public User() { }
+
+    public User(string firstName, string lastName, string username, string password)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Username = username;
+        Password = password;
+    }
+}
