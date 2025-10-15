2d9ac9e559a515b35e71ca268be254be93349ede
Gaaldra <gabrielalves.quadra@gmail.com>
2025-10-15 16:10:47 -0300

    feat(data): adiciona tabela de Users
    Adicionado a tabela de Users através da propriedade DbSet<User>.
    
    Configurado as colunas da tabela Users com base nas propriedades da entidade User.

--- Doily.API/Data/DoilyContext.cs
+++ Doily.API/Data/DoilyContext.cs
@@ -1,8 +1,20 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Doily.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Doily.API.Data;
 
 public class DoilyContext(DbContextOptions dbContextOptions): DbContext(dbContextOptions)
 {
+    public DbSet<User> Users { get; set; }
 
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        // Criando um PK (Primary Key)
+        modelBuilder.Entity<User>().HasKey(u => u.ID);
+        // Definindo colunas obrigatórias
+        modelBuilder.Entity<User>().Property(u => u.FirstName).IsRequired();
+        modelBuilder.Entity<User>().Property(u => u.LastName).IsRequired();
+        // Definindo colunas unicas
+        modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
+    }
 }
