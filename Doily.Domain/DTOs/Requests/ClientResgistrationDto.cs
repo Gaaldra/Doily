6033228cc7f1cc2d10327b3f5f055d4d65ed3331
Gaaldra <gabrielalves.quadra@gmail.com>
2025-10-15 15:26:55 -0300

    feat(dto): adiciona classes para requisição e resposta
    Foram adicionados duas novas classes ao projeto: `ClientRegistrationDto`  e `ClientResponseDto`. Essas classes serão utilizadas para representar os dados de entrada e saída relacionados ao cliente.

---
+++ Doily.Domain/DTOs/Requests/ClientResgistrationDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Doily.Domain.DTOs.Requests;
+
+public class ClientResgistrationDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+}
---
+++ Doily.Domain/DTOs/Responses/ClientResponseDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Doily.Domain.DTOs.Responses;
+
+public class ClientResponseDto
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Username { get; set; } = string.Empty;
+}
