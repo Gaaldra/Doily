6ffb491016db2a648830896a845c2114b2b6d699
Gaaldra <gabrielalves.quadra@gmail.com>
2025-10-15 16:58:16 -0300

    feat(dbcontext): adiciona configuração para entidade TaskItem
    Adicionado configurações para as colunas da tabela `Tasks` com restrições exigidas.
    
    Adicionado uma chave estrangeira a coluna `Tasks` que referencia à coluna `ID` da tabela `Users`.

--- Doily.API/Data/DoilyContext.cs
+++ Doily.API/Data/DoilyContext.cs
@@ -6,15 +6,49 @@ namespace Doily.API.Data;
 public class DoilyContext(DbContextOptions dbContextOptions): DbContext(dbContextOptions)
 {
     public DbSet<User> Users { get; set; }
+    public DbSet<TaskItem> Tasks { get; set; }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
+        // Entidade User
         // Criando um PK (Primary Key)
-        modelBuilder.Entity<User>().HasKey(u => u.ID);
+        modelBuilder.Entity<User>()
+            .HasKey(u => u.ID);
         // Definindo colunas obrigatórias
-        modelBuilder.Entity<User>().Property(u => u.FirstName).IsRequired();
-        modelBuilder.Entity<User>().Property(u => u.LastName).IsRequired();
+        modelBuilder.Entity<User>()
+            .Property(u => u.FirstName)
+            .IsRequired();
+
+        modelBuilder.Entity<User>()
+            .Property(u => u.LastName)
+            .IsRequired();
         // Definindo colunas unicas
-        modelBuilder.Entity<User>().HasIndex(u => u.Username).IsUnique();
+        modelBuilder.Entity<User>()
+            .HasIndex(u => u.Username)
+            .IsUnique();
+
+        // Entidade Task
+        modelBuilder.Entity<TaskItem>()
+            .HasKey(t => t.ID);
+
+        // Definido colunas obrigatórias
+        modelBuilder.Entity<TaskItem>()
+            .Property(t => t.Title)
+            .IsRequired();
+
+        modelBuilder.Entity<TaskItem>()
+            .Property(t => t.Description)
+            .IsRequired();
+
+        // Definindo valor padrão
+        modelBuilder.Entity<TaskItem>()
+            .Property(t => t.IsCompleted)
+            .HasDefaultValue(false);
+
+        // Definindo chave estrangeira
+        modelBuilder.Entity<TaskItem>()
+            .HasOne(t => t.User)
+            .WithMany(u => u.Tasks)
+            .HasForeignKey(t => t.UserID);
     }
 }
